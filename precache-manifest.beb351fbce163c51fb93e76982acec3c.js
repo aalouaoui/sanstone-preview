d4d055cdbfaa7ea020077b470531e4b137cba26d
aalouaoui <khalidaalouaoui@gmail.com>
2019-10-04 00:22:00 +0100

    preview

---
+++ precache-manifest.beb351fbce163c51fb93e76982acec3c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c135c84515aa3e07379ed7d1c87861",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7ce578d5104128c511e6",
+    "url": "./static/css/main.406ae3b8.chunk.css"
+  },
+  {
+    "revision": "9a6208a42bebe121a7e9",
+    "url": "./static/js/2.7843015e.chunk.js"
+  },
+  {
+    "revision": "7ce578d5104128c511e6",
+    "url": "./static/js/main.fadbfcf9.chunk.js"
+  },
+  {
+    "revision": "017b4e5724d75dd2b7db",
+    "url": "./static/js/runtime~main.7ea9aa8a.js"
+  },
+  {
+    "revision": "3296ba303209d33096209094ba8441ed",
+    "url": "./static/media/bg.3296ba30.png"
+  },
+  {
+    "revision": "8344e02b9bef4049602cddd2d7ff210e",
+    "url": "./static/media/gothambold.8344e02b.woff"
+  },
+  {
+    "revision": "9c64f7144d7b3bb248c39eaafcb92c5f",
+    "url": "./static/media/gothambold.9c64f714.woff2"
+  },
+  {
+    "revision": "24cc83428ab46b62c06efaf5532ad9a1",
+    "url": "./static/media/gothambook.24cc8342.woff"
+  },
+  {
+    "revision": "4d52df868d0fc1c0e9f454a5df820040",
+    "url": "./static/media/gothambook.4d52df86.woff2"
+  },
+  {
+    "revision": "1fc1e9950263d99b287f51470248b391",
+    "url": "./static/media/gothamlight.1fc1e995.woff2"
+  },
+  {
+    "revision": "e7a16fa4bf26845b8557804d8501bfb7",
+    "url": "./static/media/gothamlight.e7a16fa4.woff"
+  },
+  {
+    "revision": "2a89de81355012cb54e8c5417ca87645",
+    "url": "./static/media/gothamlightitalic.2a89de81.woff"
+  },
+  {
+    "revision": "efed9220c3c73b278e6a6c8b660323bb",
+    "url": "./static/media/gothamlightitalic.efed9220.woff2"
+  },
+  {
+    "revision": "851676293ad7b2d4bb755367e226a428",
+    "url": "./static/media/gothammedium.85167629.woff2"
+  },
+  {
+    "revision": "ee9835fa42a2b34483eee0db0004a13a",
+    "url": "./static/media/gothammedium.ee9835fa.woff"
+  },
+  {
+    "revision": "3a2acb5c19e4d718c1bde16c806de90b",
+    "url": "./static/media/icomoon.3a2acb5c.ttf"
+  },
+  {
+    "revision": "683abb3520b87682855e43c0cf1aabc0",
+    "url": "./static/media/icomoon.683abb35.svg"
+  },
+  {
+    "revision": "802a0ac91fbd43d7ce4c92b249e26d39",
+    "url": "./static/media/icomoon.802a0ac9.woff"
+  },
+  {
+    "revision": "ab0f922a75bc45c17be0f2dcc21e7c41",
+    "url": "./static/media/icomoon.ab0f922a.eot"
+  }
+]);
